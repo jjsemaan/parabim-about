fb935e4ef26c6a5700f0ece5c2947dc3c01f6b2e
jjsemaan <>
2023-10-17 19:05:10 +0000

    Modify js-particles responsiveness.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ particlesJS("particles-js", {
             "value": 480,
             "density": {
                 "enable": true,
-                "value_area": 0
+                "value_area": 2000
             }
         },
         "color": {
